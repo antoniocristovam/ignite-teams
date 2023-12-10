e0c5a1d04d7e85ed71eec4e6f72a75bdbdba8dcd
antoniocristovam <antoniocristovam@outlook.com.br>
2023-12-10 01:05:18 -0300

    feat: add theme defauld aplcationi

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import theme from "@theme/index";
 import { Groups } from "@screens/groups";
+import { ThemeProvider } from "styled-components";
 
 export default function App() {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
+  );
 }
