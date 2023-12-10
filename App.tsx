d663cbe675d7fe25274fa8bc726cd3e1cdd682c9
antoniocristovam <antoniocristovam@outlook.com.br>
2023-12-10 01:46:31 -0300

    feat render condition font

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
+import { ActivityIndicator } from "react-native";
 import React from "react";
 import theme from "@theme/index";
 import { Groups } from "@screens/groups";
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
