5f80bc448a7e925edcdef96ff8b9db2707b31db5
antoniocristovam <antoniocristovam@outlook.com.br>
2023-12-12 19:35:36 -0300

    feat: create component Button

---
+++ src/components/button/index.tsx
@@ -0,0 +1,16 @@
+import { TouchableOpacityProps } from "react-native";
+import { Container, Title, ButtonTypeStylesProps } from "./styles";
+import React from "react";
+
+type Props = TouchableOpacityProps & {
+  title: string;
+  type?: ButtonTypeStylesProps;
+};
+
+export function Button({ title, type = "PRIMARY", ...rest }: Props) {
+  return (
+    <Container type={type} {...rest}>
+      <Title>{title}</Title>
+    </Container>
+  );
+}
--- src/components/listInput/styles.ts
+++ src/components/listInput/styles.ts
@@ -10,6 +10,6 @@ export const Container = styled.View`
 export const Message = styled.Text`
   text-align: center;
   font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
   color: ${({ theme }) => theme.COLORS.GRAY_300};
 `;
