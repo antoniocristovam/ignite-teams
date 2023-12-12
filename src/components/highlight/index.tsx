83f08ed6bfa2200be2e55c1b7ba16d1c2bbc068a
antoniocristovam <antoniocristovam@outlook.com.br>
2023-12-11 21:31:50 -0300

    feat: add group card

---
+++ src/components/highlight/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Title, SubTitle } from "./styles";
+
+type Props = {
+  title: string;
+  subtitle: string;
+};
+
+export function Highlight({ subtitle, title }: Props) {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <SubTitle>{subtitle}</SubTitle>
+    </Container>
+  );
+}
--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Container } from "./styles";
 import { Header } from "@components/header";
+import { Highlight } from "@components/highlight";
+import { GroupCard } from "@components/groupCard";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+      <GroupCard title="Galera do ignite" />
     </Container>
   );
 }
