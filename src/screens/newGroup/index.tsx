20dc6b810c80c6e6c7c296594de28caffb31d1a7
antoniocristovam <antoniocristovam@outlook.com.br>
2024-01-13 19:12:16 -0300

    feat: adjustment import newGroup

--- src/screens/newGroup/index.tsx
+++ src/screens/newGroup/index.tsx
@@ -1,9 +1,14 @@
+//import
 import React from "react";
+
+//Styles
+import { Container, Content, Icon } from "./styles";
+
+// Component
 import { Input } from "@components/input";
 import { Button } from "@components/button";
 import { Header } from "@components/header";
 import { Highlight } from "@components/highlight";
-import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
   return (
