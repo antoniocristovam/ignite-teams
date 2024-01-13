24d4bdf786d9087d27c82e65bc8da210a0338c83
antoniocristovam <antoniocristovam@outlook.com.br>
2024-01-13 19:09:09 -0300

    feat: adjustment import buttonIcon

--- src/components/buttonIncon/styles.ts
+++ src/components/buttonIncon/styles.ts
@@ -1,5 +1,5 @@
-import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
+import { TouchableOpacity } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 export type ButtonIconTypeStylesProps = "PRIMARY" | "SECONDARY";
