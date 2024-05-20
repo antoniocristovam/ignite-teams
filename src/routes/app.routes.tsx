941783d70d9dca7a58483e4e9b33b5663adf6c4a
antoniocristovam <antoniocristovam@outlook.com.br>
2024-05-20 19:33:09 -0300

    feat: add router

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Groups } from "@screens/groups";
+import { Players } from "@screens/players";
+import { NewGroup } from "@screens/newGroup";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+  return (
+    <Navigator>
+      <Screen name="groups" component={Groups} />
+      <Screen name="newGroups" component={NewGroup} />
+      <Screen name="players" component={Players} />
+    </Navigator>
+  );
+}
