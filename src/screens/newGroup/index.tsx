ea3aa139865302864a565facf6768e0c13f6d378
antoniocristovam <antoniocristovam@outlook.com.br>
2024-05-22 20:57:02 -0300

    feat: verify space void

--- src/screens/newGroup/index.tsx
+++ src/screens/newGroup/index.tsx
@@ -1,4 +1,5 @@
 //import
+import { Alert } from "react-native";
 import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 
@@ -7,6 +8,7 @@ import { Container, Content, Icon } from "./styles";
 
 // Component
 import { Input } from "@components/input";
+import { AppError } from "@utils/AppError";
 import { Button } from "@components/button";
 import { Header } from "@components/header";
 import { Highlight } from "@components/highlight";
@@ -18,9 +20,18 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Nome do grupo não pode ser vazio.");
+      }
+
       await groupCreate(group);
       navigation.navigate("players", { group });
     } catch (err) {
+      if (err instanceof AppError) {
+        Alert.alert("Novo Grupo", err.message);
+      } else {
+        Alert.alert("Novo Grupo", "Erro ao criar novo grupo.");
+      }
       console.log(err);
     }
   }
